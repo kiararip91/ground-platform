3cb0e2b2a3681e8356df76851170f0370cacd595
Chiara <chiara.ripanti@gmail.com>
2019-06-21 14:26:20 +0100

    Delete firebase-config.js

--- web/src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDRgRcMtQ6Z7Y9wdlvQuPqIf7eSivZJUYQ",
-    authDomain: "chiara-sandbox.firebaseapp.com",
-    databaseURL: "https://chiara-sandbox.firebaseio.com",
-    projectId: "chiara-sandbox",
-    storageBucket: "chiara-sandbox.appspot.com",
-    messagingSenderId: "1045655534139",
-    appId: "1:1045655534139:web:156f86dc5b1ba449"
-  };
